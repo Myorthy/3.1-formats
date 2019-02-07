9f6a010ba2a6929b31c59fe0a7a7e1fa0493bd38
Myorthy <46349046+Myorthy@users.noreply.github.com>
2019-02-08 01:37:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,29 @@ def top_10(items):
   repeat(all_words)
   return all_words[:10]
 
-
+# for json
 with open("newsafr.json") as datafile:
-    json_data = json.load(datafile)
-    print(top_10(json_data))
+  json_data = json.load(datafile)
+  print(top_10(json_data))
      
+# for xml
+import xml.etree.ElementTree as ET
+tree = ET.parse("newsafr.xml")     
+
+def top_10x(items):
+  all_words = list()
+  for item in items:
+    words = item["description"].split()
+    for word in words:
+      if len(word) < 6:
+        words.remove(word)
+    all_words.extend(words)
+  all_words = sorted(all_words, key=sortByLength)
+  all_words.reverse()
+  repeat(all_words)
+  return all_words[:10]
+
+root = tree.getroot()
+xml.root = root.findall("channel/item")
+print(top_10x(xml.root))
+
